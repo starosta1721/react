642f449971ad96500c8172dd22a0179329997341
starosta1721 <starosta1721@gmail.com>
2020-01-28 12:41:30 +0200

    simple react based weather app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PLACES = [
+  {name: "Kharkiv", zip: '61000'},
+  {name: "Kyiv", zip: '01000'},
+  {name: "Lviv", zip: '79007'},
+];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0,
+    };
+  };
+  
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+              {place.name}
+          </button>
+        ))}
+        <WeatherDisplay
+          key={activePlace}
+          zip={PLACES[activePlace].zip}
+        />
+      </div>
+    );
+  };
 }
 
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null,
+    }
+  }
+
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    zip +
+    "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
+  render() {
+    const weatherData = this.state.weatherData;
+    const data = JSON.stringify(weatherData);
+    const data1 = weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    return (
+    <div>
+    <p>{"Weather: " + data1.weather[0].description}</p>
+    <p>{"Wind speed: " + data1.wind.speed + ' m/hr'}</p>
+    </div>  
+    )
+  }
+}
 export default App;
